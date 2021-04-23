afa7990e02701d2ddc7f78dcae67d86301817676
Ciprian Grigor <ciprian.grigor@yopeso.com>
2021-04-23 18:37:02 +0300

    Use mockk instead of mockito

--- app/src/test/java/com/example/android/dagger/login/LoginViewModelTest.kt
+++ app/src/test/java/com/example/android/dagger/login/LoginViewModelTest.kt
@@ -19,60 +19,58 @@ package com.example.android.dagger.login
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.android.dagger.LiveDataTestUtil
 import com.example.android.dagger.user.UserManager
-import org.junit.Assert.assertEquals
-import org.junit.Before
+import com.google.common.truth.Truth.assertThat
+import io.mockk.confirmVerified
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.ArgumentMatchers.anyString
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
-import org.mockito.Mockito.`when` as whenever
 
 class LoginViewModelTest {
 
     // Executes each task synchronously using Architecture Components.
-    @get:Rule var instantExecutorRule = InstantTaskExecutorRule()
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
-    private lateinit var viewModel: LoginViewModel
-    private lateinit var userManager: UserManager
-
-    @Before
-    fun setup() {
-        userManager = mock(UserManager::class.java)
-        viewModel = LoginViewModel(userManager)
-    }
+    private val userManager = mockk<UserManager>()
+    private val viewModel = LoginViewModel(userManager)
 
     @Test
     fun `Get username`() {
-        whenever(userManager.username).thenReturn("Username")
+        every { userManager.username } returns "Username"
 
         val username = viewModel.getUsername()
 
-        assertEquals("Username", username)
+        assertThat(username).isEqualTo("Username")
     }
 
     @Test
     fun `Login emits success`() {
-        whenever(userManager.loginUser(anyString(), anyString())).thenReturn(true)
+        every { userManager.loginUser(any(), any()) } returns true
 
         viewModel.login("username", "login")
 
-        assertEquals(LiveDataTestUtil.getValue(viewModel.loginState), LoginSuccess)
+        assertThat(LiveDataTestUtil.getValue(viewModel.loginState)).isEqualTo(LoginSuccess)
     }
 
     @Test
     fun `Login emits error`() {
-        whenever(userManager.loginUser(anyString(), anyString())).thenReturn(false)
+        every { userManager.loginUser(any(), any()) } returns false
 
         viewModel.login("username", "login")
 
-        assertEquals(LiveDataTestUtil.getValue(viewModel.loginState), LoginError)
+        assertThat(LiveDataTestUtil.getValue(viewModel.loginState)).isEqualTo(LoginError)
     }
 
     @Test
     fun `Login unregisters`() {
+        every { userManager.unregister() } returns Unit
+
         viewModel.unregister()
 
-        verify(userManager).unregister()
+
+        //verify { userManager.unregister() }
+
+        confirmVerified(userManager)
     }
 }
